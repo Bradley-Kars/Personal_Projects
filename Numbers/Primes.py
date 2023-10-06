d47746ba1d0992a448ed33e7a173cb73eeb9454e
Bradley Kars <Bradley.kars@gmail.com>
2023-10-06 14:49:09 -0400

    update

---
+++ Numbers/Primes.py
@@ -0,0 +1,23 @@
+primes = []
+
+amount = int(input("How many prime numbers would you like to generate?: "))
+
+if amount >= 1:
+    primes.append(2)
+    num = 3 
+
+    while len(primes) < amount:
+        is_prime = True
+
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+
+        if is_prime:
+            primes.append(num)
+
+        num += 2 
+
+for prime in primes:
+    print(prime)
